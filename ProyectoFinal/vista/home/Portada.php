6053fe6ffa8afd6da66dde982dc8163410d799d6
MarjorieFalcone <60684508+MarjorieFalcone@users.noreply.github.com>
2022-03-14 15:03:49 -0500

    1

---
+++ ProyectoFinal/vista/home/Portada.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <?php require_once 'vista/templates/encabezado.php'; ?>
+    <body>      
+        <div style="align-items: center; text-align: center; border-style: dashed; border-color: pink; margin: 20px;">
+            <div class="row" style="text-align: center; display: flex; align-items: center;
+             justify-content: center;">
+                
+		<div class="elemento col-md-3" style="background:pink; padding:10px; margin: 5px; border: 1px solid lightblue">
+		    <img src="assets_MF/image/logo.png" alt="imagen_LM">
+		</div >
+				
+		<div class="elemento col-md-5" style="padding:10px; margin: 5px; border: 1px solid pink">
+                    <p><b>Comunidad:</b><br>
+                Jóvenes comprometidos a brindarles creatividad, exclusividad y calidad a 
+                todos nuestros clientes a través de nuestros arreglos florales para toda ocasión.</p>
+		</div>	
+		<div class="elemento col-md-3" style="background: pink; padding:10px; margin: 5px; border: 1px solid lightblue">
+		<img src="assets_MF/image/logo.png" alt="imagen_LM" width="200" height="100">
+		</div >
+                
+	    </div>
+
+            <div class="row" style="display: flex; align-items: center;
+             justify-content: center;">		
+	    <div class="elemento col-md-11" style="border: 1px solid pink; margin: 5px;">	
+                <img class="Portada" src="assets_MF/image/Portada.png" alt="imagen_LM">
+	    </div>	
+                	
+                
+	    </div>
+            
+            <div class="row" style="text-align: center; display: flex; align-items: center;
+             justify-content: center;">
+                
+		<div class="elemento col-md-3" style="background:pink; padding:10px; margin: 5px; border: 1px solid lightblue">
+		    <img src="assets_MF/image/logo.png" alt="imagen_LM" width="200" height="100">
+		</div >
+				
+		<div class="elemento col-md-5" style="padding:10px; margin: 5px; border: 1px solid pink">
+		<p>Somos una florería boutique pionera en innovación y servicio al cliente con el más
+                   alto compromiso de calidad en todos nuestros productos y servicios. 
+                   Puedes usar la forma de pago que mejor se adapte a tus necesidades.</p>
+		</div>	
+		<div class="elemento col-md-3" style="background:pink; padding:10px; margin: 5px; border: 1px solid lightblue">
+		<img src="assets_MF/image/logo.png" alt="imagen_LM" width="200" height="100">
+		</div >
+                
+	    </div>
+            
+        </div>
+        
+     
+   
+    </body>
+    <?php require_once 'vista/templates/piedepagina.php'; ?>
+</html>
